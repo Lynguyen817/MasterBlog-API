4703426afd958230179c3036df370cc11cc8c24f
Lynguyen817 <lynguyen1708@gmail.com>
2023-07-06 21:17:48 -0400

    Add json file

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,41 +1,42 @@
-from flask import Flask, jsonify, request, redirect, render_template, url_for
+from flask import Flask, jsonify, url_for, request, redirect, render_template, url_for
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
-
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
-    return jsonify(POSTS)
+    """Returns a list of posts to a template for display."""
+    with open("posts.json", "r") as file:
+        posts = json.load(file)
+    return jsonify(posts)
 
-@app.route('/add', methods=['GET', 'POST'])
+
+@app.route('/api/posts', methods=['POST'])
 def add():
-    """ Add new blog_post to the list if a Post request is sent.
-        Display a form for creating a new blog_post if Get request is set.
-    """
+    """ Add new blog_post to the list if a Post request is sent."""
     blog_posts = []
     if request.method == 'POST':
-        # Get the id, author, title, content from the form
-        id = request.form['id']
-        author = request.form['author']
+        # Get the title, content from the form
         title = request.form['title']
-        content = request.form["content"]
+        content = request.form['content']
+        # Make sure that both title and content are provided
+        if not title or not content:
+            return jsonify({'error': 'Title and content are required'}), 400
         # Create a new blog post dictionary
         new_post = {
-            'id': id,
-            'author': author,
             'title': title,
             'content': content
         }
         blog_posts.append(new_post)
-        return redirect(url_for('index'))
-    return render_template('add.html')
+        # Save data to a newfile
+        with open('posts.json', 'w') as newfile:
+            json.dump(blog_posts, newfile, indent=4)
+        # Redirect back to homepage
+        return redirect(url_for('get_posts'))
+    return render_template('index.html')
 
 
 @app.route('/delete/<int:post_id>', methods=['POST'])
@@ -47,8 +48,6 @@ def delete(post_id):
         if post['id'] == post_id:
             blog_posts.remove(post)
     # Redirect back to the home page
-    print(blog_posts)
-    print(redirect(url_for('index')))
     return redirect(url_for('index'))
 
 
