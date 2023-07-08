cdd97a9b8da7659040c73e680717948b637d2328
Lynguyen817 <lynguyen1708@gmail.com>
2023-07-08 18:39:29 -0400

    Update add function

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -17,41 +17,48 @@ def get_posts():
 @app.route('/api/posts', methods=['POST'])
 def add():
     """ Add new blog_post to the list if a Post request is sent."""
-    blog_posts = []
-    if request.method == 'POST':
-        # Get the title, content from the form
-        title = request.form['title']
-        content = request.form['content']
-        # Make sure that both title and content are provided
-        if not title or not content:
-            return jsonify({'error': 'Title and content are required'}), 400
-        # Create a new blog post dictionary
-        new_post = {
-            'title': title,
-            'content': content
-        }
-        blog_posts.append(new_post)
-        # Save data to a newfile
-        with open('posts.json', 'w') as newfile:
-            json.dump(blog_posts, newfile, indent=4)
-        # Redirect back to homepage
-        return redirect(url_for('get_posts'))
-    return render_template('index.html')
-
-
-@app.route('/delete/<int:post_id>', methods=['POST'])
-def delete(post_id):
-    """ Find the blog post with the given id and remove it from the list"""
-    with open("job_posts.json", "r") as fileobj:
-        blog_posts = json.load(fileobj)
-    for post in blog_posts:
-        if post['id'] == post_id:
-            blog_posts.remove(post)
-    # Redirect back to the home page
+    try:
+        with open("posts.json", "r") as fileobj:
+            blog_posts = json.load(fileobj)
+    except FileNotFoundError:
+        return "File not found", 404
+    # Get the title, content from the json file
+    data = request.get_json()
+    title = data['title']
+    content = data['content']
+    # If missing title and content, raise an error
+    if title == "" or content == "":
+        return jsonify({'error': 'Title and content are required'}), 400
+    # Create a new blog post dictionary
+    new_post = {
+        'title': title,
+        'content': content
+    }
+    blog_posts.append(new_post)
+    # Save data to a newfile
+    with open('posts.json', 'w') as newfile:
+        json.dump(blog_posts, newfile, indent=4)
+    # Redirect back to homepage
     return redirect(url_for('index'))
 
 
-@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+@app.route('/api/posts/<post_id>', methods=['DELETE'])
+def delete(post_id):
+    """ Find the blog post with the given id and remove it from the list"""
+    with open("posts.json", "r") as fileobj:
+        posts = json.load(fileobj)
+    for post in posts:
+        if post['id'] == post_id:
+            posts.remove(post)
+        if not post_id:
+            return jsonify({'error': '400 Not Found'})
+    with open('posts.json', 'w') as newfile:
+        json.dump(posts, newfile, indent=4)
+
+    return jsonify({"message": "Post with id <id> has been deleted successfully."}), 201
+
+
+@app.route('/api/posts/<id>', methods=['PUT'])
 def update(post_id):
     """
         Fetch the blog posts from the JSON file.
@@ -64,20 +71,18 @@ def update(post_id):
         return "File not found", 404
 
     for post in blog_posts:
-        if post['id'] == post_id:
-            if request.method == 'POST':
-                # Update the post in the JSON file
-                post['author'] = request.form['author']
-                post['title'] = request.form['title']
-                post['content'] = request.form['content']
+        # Update the post in the JSON file
+        post['id'] = request.form['id']
+        post['title'] = request.form['title']
+        post['content'] = request.form['content']
 
-                with open('job_posts.json', 'w')as fileobj:
-                    json.dump(blog_posts, fileobj, indent=4)
+        with open('job_posts.json', 'w')as fileobj:
+            json.dump(blog_posts, fileobj, indent=4)
 
-            # Redirect back to index
-            return redirect(url_for('index'))
-        # Else, it's a GET request. So display the update.html page
-        return render_template('update.html', post=post)
+    # Redirect back to index
+    return redirect(url_for('index'))
+    # Else, it's a GET request. So display the update.html page
+    #return render_template('update.html', post=post)
 
 
 if __name__ == '__main__':
